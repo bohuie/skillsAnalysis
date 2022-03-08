11a28b65d42d200cb2dc769b45947ff353055bd3
Hongaponga <72040706+liewshanhong@users.noreply.github.com>
2022-03-08 01:06:36 -0800

    fixed question response

--- skills_DS/base/models.py
+++ skills_DS/base/models.py
@@ -54,7 +54,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE, related_name="profile")
     age = models.IntegerField(null=True)
     gender = models.CharField(max_length=20)
-    yearOfStudy = models.IntegerField(null=True)
+    yearOfStudy = models.CharField(max_length=2)
     skills = models.TextField()
 
    
