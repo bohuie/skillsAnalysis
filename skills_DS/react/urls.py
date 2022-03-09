02b6168a5bca851f514524b4106d72bca049bc4a
Brian Z <brianzhouzc@gmail.com>
2022-03-09 13:19:33 -0800

    Updated urls for matching string without ending slashes

--- skills_DS/react/urls.py
+++ skills_DS/react/urls.py
@@ -1,8 +1,8 @@
-from django.urls import re_path, path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path("upload", views.login_required),
-    path("profile", views.login_required, name="user_profile"),
+    re_path(r"^upload\/?$", views.login_required_view, name="resume_upload"),
+    re_path(r"^profile\/?$", views.profile_required_view, name="user_profile"),
     re_path(r"^.*", views.index) 
 ]
--- skills_DS/react/views.py
+++ skills_DS/react/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
+from base.models import Profile
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'react/index.html')
 
-
-def login_required(request):
+def login_required_view(request):
 	if request.user.is_authenticated:
 		return render(request, 'react/index.html')
+	else:
+		return redirect("login")
+
+def profile_required_view(request):
+	if request.user.is_authenticated:
+		if Profile.objects.filter(user = request.user).exists():
+			return render(request, 'react/index.html')
+		else:
+			return redirect("questions")
 	else:
 		return redirect("login")
